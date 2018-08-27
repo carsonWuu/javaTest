171b93a887d9886da41f2d7fdcbbb00bbe349d3b
carsonWuu <3263418180@qq.com>
2018-08-27 10:40:08 +0800

    init

--- src/main_classTostring.java
+++
@@ -1,9 +0,0 @@
-
-public class main_classTostring {
-	
-	public static void main(String args[]){
-		classTostring cl=new classTostring(1,2,"hello");
-		System.out.println(cl);
-	}
-	
-}
--- src/strign.java
+++
@@ -1,9 +0,0 @@
-
-public class strign {
-	public static void main(String []args){
-		for(int i=0;i<100;i++){
-			String temp="INSERT INTO iov_track_"+i+" (c_imei,n_clng,n_clat,n_lat,n_lng,t_data_time) select c_imei,n_clng,n_clat,n_lat,n_lng,t_data_time from iov_track_"+i+";";
-			System.out.println(temp);
-		}
-	}
-}
